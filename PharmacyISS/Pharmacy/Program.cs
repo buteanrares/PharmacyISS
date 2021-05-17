268e72ed3f2c8baf0662f3a63f146b2b81d77f67
Rares Butean <59267118+buteanrares@users.noreply.github.com>
2021-05-17 17:21:39 +0300

    Added Generic repository Interface and Base Class. Implemented Specific repositories

--- PharmacyISS/Pharmacy/Program.cs
+++ PharmacyISS/Pharmacy/Program.cs
@@ -1,3 +1,4 @@
+using Pharmacy.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ PharmacyISS/Pharmacy/Repository/Repositories.cs
@@ -0,0 +1,19 @@
+﻿using Pharmacy.Domain;
+
+namespace Pharmacy.Repository
+{
+    class AccountRepository : GenericRepository<Account>
+    {
+        public AccountRepository() : base("Account") { }
+    }
+
+    class EmployeeRepository : GenericRepository<Employee>
+    {
+        public EmployeeRepository() : base("Employee") { }
+    }
+
+    class MedicineRepository : GenericRepository<Medicine>
+    {
+        public MedicineRepository() : base("Medicine") { }
+    }
+}
